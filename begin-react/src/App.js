0c61b9a1bf1cd9fea76f293cd64527a1166e2e3b
Sehui Jeong <46295659+SayisMe@users.noreply.github.com>
2023-06-15 11:37:13 +0900

    jsx 개념

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hello from "./Hello";
+import './App.css'
 
 function App() {
+  const name = 'ff';
+  const style = {
+    backgroundColor: '#fffff',
+    color: 'red',
+    fontSize: 28,
+    padding: '3rem',
+    margin: '1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-box" >
+      {/* */}
+      <div style = {style}>{name}</div>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 }
